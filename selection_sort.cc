2511628f626b4b18a1ce114d847c067914e29a7d
Kellian <theunmovablelibrary@gmail.com>
2017-01-02 23:27:35 -0800

    added selection_sort.cc

--- selection_sort.cc
+++ selection_sort.cc
@@ -6,7 +6,7 @@ void selection_sort(int array[], int n){
 	for (int i = 0; i < n - 1; ++i){
 		minIndex = i;
 		for (int j = i + 1; j < n; ++j){
-			if (array[j] < array[minIndex]) //if 
+			if (array[j] < array[minIndex])
 				minIndex = j;
 			if (minIndex != i) {
 				std::swap(array[i], array[minIndex]);
